48e4187e7ae10a625472ab768154520671a8fe33
erickcion@gmail.com <erickcion@gmail.com@386c8fc6-99ef-11de-b37f-933587f4c9ea>
2010-03-13 07:48:08 +0000

    - Correccion de URL en emails enviados

--- src/application/views/mail/bienvenida.php
+++ src/application/views/mail/bienvenida.php
@@ -1,21 +1,20 @@
 <html>
 <body>
-<h1>FELICITACIONES <?php echo strtoupper($usuario->nombre. " " .$usuario->apellido)?>!</h1>
-
-<p>Te has registrado satisfactoriamente en <?php echo NOMBRE_SITIO ?>.
+<h1>FELICITACIONES <?php echo strtoupper($usuario->nombre." ".$usuario->apellido)?>!</h1>
+<p>Te has registrado satisfactoriamente en <?php echo html::anchor(url::site(), NOMBRE_SITIO) ?>.
 Ahora puedes disfrutar de nuestros servicios con solo iniciar sesion.</p>
 <p>Tus datos de acceso son los siguientes:</p>
-<table>
+<table style="margin:auto;">
 	<tr>
-		<td>USUARIO:</td>
+		<th>USUARIO:</th>
 		<td><?php echo $usuario->login ?></td>
 	</tr>
 	<tr>
-		<td>CLAVE:</td>
+		<th>CLAVE:</th>
 		<td><?php echo $usuario->clave ?></td>
 	</tr>
 </table>
-<p>En <?php echo NOMBRE_SITIO ?> trabajamos para que conseguir y
+<p>En <?php echo html::anchor(url::base(), NOMBRE_SITIO) ?> trabajamos para que conseguir y
 publicar residencias estudiantiles sea una tarea rapida y agradable.</p>
 <p>Gracias por preferirnos.</p>
 </body>
